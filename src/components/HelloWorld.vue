cc5b73c62b80b599786b046701b6f433b033acf9
AlmelK <af5909704@gmail.com>
2022-04-06 18:59:41 +0700

    first step

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,127 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="root">
+    <form class="sign-up">
+      <div class="personal-info">
+        <h2>Личные данные</h2>
+
+        <div class="form-group">
+          <label for="surName">Фамилия*</label>
+          <input type="text" id="surName" v-model="surname" />
+        </div>
+        <div class="form-group">
+          <label for="name">Имя*</label>
+          <input type="text" id="name" v-model="clientName" />
+        </div>
+        <div class="form-group">
+          <label for="third_name">Отчество</label>
+          <input type="text" id="third_name" v-model="thirdName" />
+        </div>
+        <div class="form-group">
+          <label for="birth_day">Дата рождения*</label>
+          <input type="date" id="birth_day" v-model="birthday" />
+        </div>
+        <div class="form-group">
+          <label for="phone">Номер телефона*</label>
+          <input type="tel" id="phone" v-model="phoneNumber" />
+        </div>
+        <div class="form-group">
+          <label for="chooseGender">Пол*</label>
+          <div id="chooseGender" v-for="(gend, index) in genders" :key="index">
+            <input type="radio" :value="gend.value" v-model="gender" />
+            {{ gend.gen }}
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="clientGroup">Группа клиентов*</label>
+          <select id="clientGroup" v-model="selectedGroups" multiple>
+            <option
+              v-for="(g, index) in clientsGroup"
+              :value="g.value"
+              :key="index"
+            >
+              {{ g.group }}
+            </option>
+          </select>
+        </div>
+
+        <div class="form-group">
+          <label for="doctorGroup">Лечащий врач</label>
+          <select id="doctorGroup" v-model="doctor">
+            <option 
+            v-for="(doc, index) in doctors" 
+            :value="doc.value"
+            :key="index"
+            >
+            {{ doc.doctorName }}
+            </option>
+          </select>
+        </div>
+
+        <div class="form-group">
+          <input type="checkbox" id="sms" v-model="notSendSMS">
+          <label for="sms">Не отправлять СМС.</label>
+        </div>
+
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      surname: "",
+      clientName: "",
+      thirdName: "",
+      birthday: "",
+      phoneNumber: "",
+      gender: "male",
+      genders: [
+        {
+          gen: "М",
+          value: "male",
+        },
+        {
+          gen: "Ж",
+          value: "female",
+        },
+      ],
+      clientsGroup: [
+        {
+          group: "VIP",
+          value: "VIP",
+        },
+        {
+          group: "Проблемные",
+          value: "Problem",
+        },
+        {
+          group: "ОМС",
+          value: "OMS",
+        },
+      ],
+      selectedGroups: ["OMS"],
+      doctor: "",
+      doctors: [
+        {
+          doctorName: "Иванов",
+          value: "Ivanov",
+        },
+        {
+          doctorName: "Захаров",
+          value: "Zaharov",
+        },
+        {
+          doctorName: "Чернышева",
+          value: "Chernisheva",
+        },
+      ],
+      notSendSMS: false,
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
